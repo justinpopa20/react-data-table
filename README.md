79e7025700d5cfae873aa90f4f231536821c0184
Iustinian Popa <justin.popa20@gmail.com>
2020-03-28 21:19:59 +0100

    Initial table & readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Sample table features
+https://mdbootstrap.com/plugins/jquery/table-editor/
+
+## TODO
+### Edit feature
+### Filter search
+### Sort by column
+### Pagination
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import tableData from "./dataShort.json";
 
 function App() {
+  // Hooks - check useState API docs
+  const [records, setRecords] = useState(tableData);
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  const handleAdd = () => {
+    const newRecord = {
+      name: "Enter name",
+      position: "Enter position",
+      office: "474-9934 Eget St.",
+      age: 666,
+      startDate: "Jul 28, 2020",
+      salary: "831952536-00009"
+    };
+    const recTemp = [newRecord, ...records];
+    setRecords(recTemp);
+  };
+
+  const handleEdit = () => {
+    // TODO - implement edit
+
+    console.log("edit");
+  };
+
+  const handleDelete = (rowIndex) => {
+    console.log("You want to delete: ", rowIndex);
+    const newRecords = records.filter((rec, index) => index !== rowIndex)
+    setRecords(newRecords)
+  };
+
+  const handleSelectRow = rowData => {
+    if (selectedRow && rowData.name === selectedRow.name) {
+      return setSelectedRow(null);
+    }
+    setSelectedRow(rowData);
+    console.log("You clicked on : ", rowData.name);
+  };
+
+  const renderToolbar = (
+    <div className="table-toolbar center">
+      <p>1 row selected</p>
+      <div>
+        <button
+          type="button"
+          className="btn btn-primary m-2"
+          onClick={handleAdd}
+        >
+          Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary m-2"
+          onClick={handleEdit}
+        >
+          Edit
+        </button>
+      </div>
+    </div>
+  );
+
+  const renderHead = (
+    <tr>
+      <th>Name</th>
+      <th>Position</th>
+      <th>Office</th>
+      <th>Age</th>
+      <th>Start date</th>
+      <th>Salary</th>
+      <th>Actions</th>
+    </tr>
+  );
+
+  const renderBody = records.map((rowData, index) => {
+    const isRowSelected = selectedRow && rowData.name === selectedRow.name;
+    return (
+      <tr
+        key={index}
+        onClick={() => handleSelectRow(rowData)}
+        className={isRowSelected ? "tr-color-selected" : ""}
+      >
+        <td>{rowData.name}</td>
+        <td>{rowData.position}</td>
+        <td>{rowData.office}</td>
+        <td>{rowData.age}</td>
+        <td>{rowData.startDate}</td>
+        <td>{rowData.salary}</td>
+        <td>
+          <button
+            type="button"
+            className="btn btn-danger m-2"
+            onClick={(event) => {
+              event.stopPropagation()
+              handleDelete(index)
+            }}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderToolbar}
+      <table className="table">
+        <thead>{renderHead}</thead>
+        <tbody>{renderBody}</tbody>
+      </table>
     </div>
   );
 }
